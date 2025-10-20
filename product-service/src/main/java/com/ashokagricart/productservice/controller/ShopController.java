e6255ac3944d2e675b435b1cfaaccb76f3cd6da7
ashok167 <97613146+ashok167@users.noreply.github.com>
2025-10-20 23:11:40 +0530

    Added Controller classes

---
+++ product-service/src/main/java/com/ashokagricart/productservice/controller/ShopController.java
@@ -0,0 +1,34 @@
+package com.ashokagricart.productservice.controller;
+
+import com.ashokagricart.productservice.dto.ProductResponse;
+import com.ashokagricart.productservice.dto.ShopResponse;
+import com.ashokagricart.productservice.service.ShopService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("products/v1")
+@RequiredArgsConstructor
+public class ShopController {
+
+    private final ShopService shopService;
+    // GET /api/v1/cities/1/shops?type=pesticide
+    @GetMapping("/cities/{cityId}/shops")
+    public ResponseEntity<List<ShopResponse>> getShopsByCityAndType(
+            @PathVariable Long cityId,
+            @RequestParam(name = "type", required = true) String shopType
+    ){
+        if(!"pesticide".equalsIgnoreCase(shopType)){
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(shopService.getShopsByCityAndType(cityId, shopType));
+    }
+@GetMapping("/shops/{shopId}/products")
+    public ResponseEntity<List<ProductResponse>> getProductsByShop(@PathVariable Long shopId){
+        return ResponseEntity.ok(shopService.getProductsByShop(shopId));
+}
+
+}
